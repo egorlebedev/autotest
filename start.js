e49db73f0d2ac38b0245d67812e52ab9fb1ad90b
e.lebedev <e.lebedev@ceteralabs.com>
2019-04-22 00:18:21 +0300

    Добавил очередь тестов

--- start.js
+++ start.js
@@ -2,45 +2,65 @@ const child_process = require('child_process'),
     fs = require('fs'),
     Queue = require('./models/Queue.js');
 
-let projectsArr = [];
+let configs = [];
 let porjectsPromises = [];
 const QueueObj = new Queue();
 
 fs.readdirSync("./projects").forEach(projectName => {
     if (projectName !== "PROJECT_NAME") {
-        projectsArr.push(projectName);
-        porjectsPromises.push(
-            new Promise((resolve, reject) => {
-                QueueObj.findOne({"projectName":projectName}).then(
-                    date => {
-                        console.log(data);
-                    }
-                )
-            })
+        configs[projectName] = require('./projects/'+projectName+'/config.js');
+        QueueObj.findOne({'finished':false}).then(
+            data => {
+                if (data !== null)
+                    return 0;
+
+                porjectsPromises.push(
+                    new Promise((resolve, reject) => {
+                        QueueObj.findOne({"projectName": projectName}).then(
+                            data => {
+
+                                if (configs[projectName].disabled === true)
+                                    return;
+
+                                let frequency = ((configs[projectName].tests || {}).common || {}).frequency || 86400000;
+
+                                if (data && data.dateFinish && (data.dateFinish + frequency > Date.now()))
+                                    return;
+
+                                let sitemapPath = './sitemaps/' + projectName + '.json';
+                                let sitemap = false;
+                                let sitemapExpTime;
+
+                                if (fs.existsSync(sitemapPath)){
+                                    sitemapExpTime = configs[projectName].sitemapExpTime || 86400000;
+                                    sitemap = fs.statSync(sitemapPath);
+                                }
+
+                                if (sitemap === false || sitemap.birthtimeMs + sitemapExpTime < Date.now()) {
+                                    try {
+                                        child_process.execSync('node sitemap ' + projectName);
+                                    } catch (err) {
+                                        ;
+                                    }
+                                }
+
+                                try {
+                                    child_process.execSync('node app projects/' + projectName + '/common');
+                                } catch (err) {
+                                    ;
+                                }
+                            }
+                        )
+                    })
+                );
+            }
         );
     }
 });
 
 
+Promise.all(porjectsPromises);
 
-QueueObj.findOne({'finished':false}).then(
-    data => {
-        if (data !== null)
-            return 0;
-
-        projectsArr.forEach(function (project) {
-
-            if (project !== "PROJECT_NAME") {
-                try {
-                    child_process.execSync('node app.js projects/' + project + '/common');
-                } catch (err) {
-                    ;
-                }
-
-            }
-        });
-    }
-);
 
 
 
